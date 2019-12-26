d2fba66537a7f84939d1bd4599e3502e81dd3a41
Alex Henry <alhenry@microsoft.com>
2019-12-25 22:27:26 -0500

    Initial Next.js commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Home = () => (
+  <>
+    <Head>
+      <script
+        src="https://kit.fontawesome.com/a14419ba0b.js"
+        crossOrigin="anonymous"
+      ></script>
+    </Head>
+
+  </>
+);
+
+export default Home;
